5e8beef29a96320417134eac3d1bdf60b06d02d8
kylinsoong <kylinsoong.1214@gmail.com>
2016-08-04 18:52:57 +0800

    TEIID-3825: add config module

--- teiid/src/main/java/org/wildfly/swarm/teiid/TeiidFraction.java
+++ teiid/src/main/java/org/wildfly/swarm/teiid/TeiidFraction.java
@@ -2,13 +2,13 @@ package org.wildfly.swarm.teiid;
 
 import org.wildfly.swarm.config.Teiid;
 import org.wildfly.swarm.spi.api.Fraction;
-import org.wildfly.swarm.spi.api.annotations.Configuration;
+import org.wildfly.swarm.spi.api.annotations.ExtensionClassName;
+import org.wildfly.swarm.spi.api.annotations.ExtensionModule;
+import org.wildfly.swarm.spi.api.annotations.MarshalDMR;
 
-@Configuration(
-        marshal = true,
-        extension="org.jboss.teiid",
-        extensionClassName = "org.teiid.jboss.TeiidExtension"
-)
+@ExtensionModule("org.jboss.teiid")
+@ExtensionClassName("org.teiid.jboss.TeiidExtension")
+@MarshalDMR
 public class TeiidFraction extends Teiid<TeiidFraction> implements Fraction {
 
     private static final long serialVersionUID = -6070901334377803127L;
