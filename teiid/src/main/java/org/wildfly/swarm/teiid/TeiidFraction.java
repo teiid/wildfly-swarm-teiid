dbb185efd864bf5db4281d090d6f89d1bfdafac0
kylinsoong <kylinsoong.1214@gmail.com>
2016-08-12 11:20:50 +0800

    add translator modules

--- teiid/src/main/java/org/wildfly/swarm/teiid/TeiidFraction.java
+++ teiid/src/main/java/org/wildfly/swarm/teiid/TeiidFraction.java
@@ -1,9 +1,15 @@
 package org.wildfly.swarm.teiid;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.wildfly.swarm.config.Teiid;
 import org.wildfly.swarm.config.infinispan.cache_container.EvictionComponent;
 import org.wildfly.swarm.config.infinispan.cache_container.LockingComponent;
 import org.wildfly.swarm.config.infinispan.cache_container.TransactionComponent;
+import org.wildfly.swarm.config.teiid.Translator;
 import org.wildfly.swarm.config.teiid.Transport;
 import org.wildfly.swarm.infinispan.InfinispanFraction;
 import org.wildfly.swarm.security.SecurityFraction;
@@ -24,6 +30,8 @@ public class TeiidFraction extends Teiid<TeiidFraction> implements Fraction {
     private String cacheContainerName = "teiid-cache";
     
     private SecurityFraction securityFraction;
+    
+    List<Translator> translators = new ArrayList<>();
 
     @Override
     public String preparedplanCacheInfinispanContainer() {
@@ -59,9 +67,76 @@ public class TeiidFraction extends Teiid<TeiidFraction> implements Fraction {
 
     @Override
     public void initialize(InitContext initContext) {
+        
         initContext.socketBinding(new SocketBinding("teiid-jdbc").port(31000));
         initContext.socketBinding(new SocketBinding("teiid-odbc").port(35432));
+        
+        registTranslators(translators);
+        
         Fraction.super.initialize(initContext);
+    }
+
+    /**
+     * If don't need install all translators, rewrite this method to re-register translators
+     * @param translators
+     */
+    @SuppressWarnings("rawtypes")
+    protected void registTranslators(List<Translator> translators) {
+        translators.add(new Translator("jdbc-simple").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("jdbc-ansi").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("access").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("db2").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("derby").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("h2").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("hsql").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("informix").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("metamatrix").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("mysql").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("mysql5").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("oracle").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("postgresql").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("greenplum").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("sqlserver").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("sybase").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("sybaseiq").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("teiid").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("teradata").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("modeshape").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("ingres").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("ingres93").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("intersystems-cache").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("netezza").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("file").module("org.jboss.teiid.translator.file"));
+        translators.add(new Translator("ldap").module("org.jboss.teiid.translator.ldap"));
+        translators.add(new Translator("loopback").module("org.jboss.teiid.translator.loopback"));
+        translators.add(new Translator("olap").module("org.jboss.teiid.translator.olap"));
+        translators.add(new Translator("ws").module("org.jboss.teiid.translator.ws"));
+        translators.add(new Translator("salesforce").module("org.jboss.teiid.translator.salesforce"));
+        translators.add(new Translator("salesforce-34").module("org.jboss.teiid.translator.salesforce").slot("34"));
+        translators.add(new Translator("hive").module("org.jboss.teiid.translator.hive"));
+        translators.add(new Translator("map-cache").module("org.jboss.teiid.translator.object"));
+        translators.add(new Translator("google-spreadsheet").module("org.jboss.teiid.translator.google"));
+        translators.add(new Translator("odata").module("org.jboss.teiid.translator.odata"));
+        translators.add(new Translator("sap-gateway").module("org.jboss.teiid.translator.odata"));
+        translators.add(new Translator("sap-nw-gateway").module("org.jboss.teiid.translator.odata"));
+        translators.add(new Translator("mongodb").module("org.jboss.teiid.translator.mongodb"));
+        translators.add(new Translator("cassandra").module("org.jboss.teiid.translator.cassandra"));
+        translators.add(new Translator("accumulo").module("org.jboss.teiid.translator.accumulo"));
+        translators.add(new Translator("solr").module("org.jboss.teiid.translator.solr"));
+        translators.add(new Translator("excel").module("org.jboss.teiid.translator.excel"));
+        translators.add(new Translator("impala").module("org.jboss.teiid.translator.hive"));
+        translators.add(new Translator("prestodb").module("org.jboss.teiid.translator.prestodb"));
+        translators.add(new Translator("hbase").module("org.jboss.teiid.translator.hbase"));
+        translators.add(new Translator("hana").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("vertica").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("actian-vector").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("osisoft-pi").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("odata4").module("org.jboss.teiid.translator.odata4"));
+        translators.add(new Translator("redshift").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("jpa2").module("org.jboss.teiid.translator.jpa"));
+        translators.add(new Translator("ucanaccess").module("org.jboss.teiid.translator.jdbc"));
+        translators.add(new Translator("simpledb").module("org.jboss.teiid.translator.simpledb"));
+        translators.add(new Translator("swagger").module("org.jboss.teiid.translator.swagger"));
     }
 
     @Override
@@ -75,7 +150,17 @@ public class TeiidFraction extends Teiid<TeiidFraction> implements Fraction {
         
         transport("jdbc" , t -> t.socketBinding("teiid-jdbc").protocol(Transport.Protocol.TEIID));
         
+        installTranslator();
+        
         Fraction.super.postInitialize(initContext);
+    }
+
+    //install translator
+    private void installTranslator() {
+        
+        
+        
+        translator("h2", t -> t.module("org.jboss.teiid.translator.jdbc"));
     }
 
     /**
--- translator-modules/src/main/resources/modules/readme.txt
+++
@@ -1,1 +0,0 @@
-Teiid Translator Modules
